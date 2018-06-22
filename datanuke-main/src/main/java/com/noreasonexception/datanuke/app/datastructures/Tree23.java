1ca06001adeb663a07f537cd4576406636616ae8
NoReasonException <stefannstefanou@gmail.com>
2018-06-22 03:09:00 +0300

    2-3 _search()

--- datanuke-main/src/main/java/com/noreasonexception/datanuke/app/datastructures/Tree23.java
+++ datanuke-main/src/main/java/com/noreasonexception/datanuke/app/datastructures/Tree23.java
@@ -3,6 +3,7 @@ package com.noreasonexception.datanuke.app.datastructures;
 import com.noreasonexception.datanuke.app.datastructures.interfaces.ITree;
 import com.sun.istack.internal.NotNull;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import sun.reflect.generics.tree.Tree;
 
 import java.security.InvalidParameterException;
 
@@ -29,22 +30,26 @@ public class Tree23<Key extends Comparable<Key>,Value> implements ITree<Key,Valu
     protected class Node3 extends Node2{
         private Key key2;
         private Value value2;
-        private Tree23.Node3 mid;
+        private Tree23.Node2 mid;
         public          Node3       (Key key, Value value, Key key2, Value value2) { super(key, value);this.key2 = key2;this.value2 = value2; }
         public void     setValue2   (Value value2) { this.value2 = value2; }
         public void     setKey2     (Key key2) {this.key2 = key2;}
         public void     setMid      (Node3 mid) {this.mid = mid;}
         public Value    getValue2   () { return value2; }
         public Key      getKey2     () { return key2; }
-        public Node3    getMid()    { return mid; }
+        public Node2    getMid()    { return mid; }
     }
 
     protected static boolean isNode3(Tree23.Node2 node2){
         return node2 instanceof Tree23.Node3;
     }
+    protected static Tree23.Node3 convertToNode3(Tree23.Node2 node2){
+        return isNode3(node2)?(Tree23.Node3)node2:null;
+    }
     @Override
     public void insert(Key k, Value v) {
-        throw new NotImplementedException();
+        if(root==null)root=new Node2(k,v);
+        else throw new NotImplementedException();
     }
 
     @Override
@@ -55,13 +60,23 @@ public class Tree23<Key extends Comparable<Key>,Value> implements ITree<Key,Valu
 
     @Override
     public Value search(Key k) {
-        Node2 node2=_search(k);
+        Node2 node2=_search(root,k);
         if(node2.getKey().compareTo(k)==0)return node2.value;
         else if(isNode3(node2))return ((Node3)node2).getValue2();
         throw new InvalidParameterException("neither node 2 or 3 ...?");
     }
-    protected Node2 _search(Key k){
-        throw new NotImplementedException();
+
+    @SuppressWarnings("unchecked")
+    protected Node2 _search(Node2 curr,Key k){
+        Tree23.Node3 node3ref;
+        int compare;
+        if((compare=curr.getKey().compareTo(k))==0)return curr;
+        else if(compare<0)return _search(curr.getLeft(),k);
+        if((node3ref=convertToNode3(curr))!=null){
+            if(node3ref.getKey2().compareTo(k)<-1) return _search(node3ref.getMid(),k);
+        }
+        return _search(curr.getRight(),k);
+
 
     }
 
@@ -69,5 +84,9 @@ public class Tree23<Key extends Comparable<Key>,Value> implements ITree<Key,Valu
     public Value pollMin() {
         throw new NotImplementedException();
 
+    }
+
+    public Tree23() {
+        this.root = null;
     }
 }
