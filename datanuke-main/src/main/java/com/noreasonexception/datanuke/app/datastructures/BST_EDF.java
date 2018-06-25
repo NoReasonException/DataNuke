bd51f4a595508f6b2ce8a42e109de1445bfb7ad5
noReasonException <stefannstefanou@gmail.com>
2018-06-25 22:18:09 +0300

    bugfix -> unnessessary overrides comes from older implementations

--- datanuke-main/src/main/java/com/noreasonexception/datanuke/app/datastructures/BST_EDF.java
+++ datanuke-main/src/main/java/com/noreasonexception/datanuke/app/datastructures/BST_EDF.java
@@ -4,23 +4,6 @@ import com.noreasonexception.datanuke.app.datastructures.interfaces.ITree;
 import com.noreasonexception.datanuke.app.threadRunner.etc.ClassInfo;
 
 public class BST_EDF extends BinarySearchTree<Long,ClassInfo>implements ITree<Long,ClassInfo> {
-
-    @Override
-    public void insert(Long aLong, ClassInfo classInfo) {
-        super.insert(aLong,classInfo);
-    }
-
-    @Override
-    public ClassInfo search(Long aLong) {
-
-        return search(aLong);
-    }
-
-    @Override
-    public void delete(Long aLong) {
-
-    }
-
     @Override
     public ClassInfo pollMin() {
         Node min;
