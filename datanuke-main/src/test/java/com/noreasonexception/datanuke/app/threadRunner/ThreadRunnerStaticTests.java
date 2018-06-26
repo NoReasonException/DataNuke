81842ce6c7fa7047a5333d723199c99b511b1fd2
noReasonException <stefannstefanou@gmail.com>
2018-06-26 12:38:30 +0300

    some tests

--- datanuke-main/src/test/java/com/noreasonexception/datanuke/app/threadRunner/ThreadRunnerStaticTests.java
+++ datanuke-main/src/test/java/com/noreasonexception/datanuke/app/threadRunner/ThreadRunnerStaticTests.java
@@ -1,7 +1,10 @@
 package com.noreasonexception.datanuke.app.threadRunner;
 
+import com.noreasonexception.datanuke.app.threadRunner.error.ThreadRunnerStaticTestException;
 import com.noreasonexception.datanuke.app.threadRunner.etc.ClassInfo;
+import junit.framework.TestFailure;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import static org.junit.Assert.*;
 
@@ -10,29 +13,37 @@ import java.util.Date;
 
 public class ThreadRunnerStaticTests {
     @Test
-    public void millsToSec(){
+    public void millsToSecTest(){
         assertEquals(AbstractThreadRunner.millsToSec(1000),1);
     }
 
     @Test
-    public void secToMills(){
+    public void secToMillsTest(){
         assertEquals(AbstractThreadRunner.secToMills(1),1000);
 
     }
 
     @Test
-    public void getRemainingTime(){
+    public void getRemainingTimeTest() throws ThreadRunnerStaticTestException{
+        if(AbstractThreadRunner.getRemainingTime(System.currentTimeMillis()+1500)<1400){
+            throw new ThreadRunnerStaticTestException("getRemainingTime failed in the .getRemainingTimeTest() Test");
+        }
     }
 
     @Test
-    public void getDeadline(){
+    public void getDeadlineTest(){
+        long i;
+        assertEquals(AbstractThreadRunner.getDeadline((i=System.currentTimeMillis())-1500,i,500),
+                i+500);
     }
 
 
 
     @Test
-    public void getWaitTime(){
-
+    public void getWaitTimeTest(){
+        long i;
+        assertEquals(AbstractThreadRunner.getWaitTime((i=System.currentTimeMillis())-1500,i,500),
+                500);
     }
 
 }
---
+++ datanuke-main/src/test/java/com/noreasonexception/datanuke/app/threadRunner/error/ThreadRunnerStaticTestException.java
@@ -0,0 +1,7 @@
+package com.noreasonexception.datanuke.app.threadRunner.error;
+
+public class ThreadRunnerStaticTestException extends Exception {
+    public ThreadRunnerStaticTestException(String message) {
+        super(message);
+    }
+}
