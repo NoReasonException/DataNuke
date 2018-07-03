0b95d661b7171c6c6e59a0b37c20a54bb194b6ae
noReasonException <stefannstefanou@gmail.com>
2018-07-03 03:08:14 +0300

    event MORE classes addedp2

---
+++ datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A28_MarkitEconomics_IHSMarkitCipsUkManufacturingPMI_UK.java
@@ -0,0 +1,28 @@
+package com.noreasonexception.loadable.childs;
+
+import com.noreasonexception.datanuke.app.ValueFilter.AbstractValueFilter;
+import com.noreasonexception.datanuke.app.threadRunner.ThreadRunnerTaskEventsDispacher;
+import com.noreasonexception.loadable.base.HtmlParser;
+
+import java.util.regex.Pattern;
+
+public class A28_MarkitEconomics_IHSMarkitCipsUkManufacturingPMI_UK extends HtmlParser {
+    public A28_MarkitEconomics_IHSMarkitCipsUkManufacturingPMI_UK(ThreadRunnerTaskEventsDispacher disp, AbstractValueFilter<Double> valueFilter) {
+        super(disp, valueFilter);
+    }
+
+    @Override
+    protected Pattern onPatternLoad() {
+        return null;
+    }
+
+    @Override
+    protected String onUrlLoad() {
+        return null;
+    }
+
+    @Override
+    protected Double onValueExtract(String tmpString) {
+        return null;
+    }
+}
---
+++ datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A32_EcEuropa_GDP_EUR.java
@@ -0,0 +1,28 @@
+package com.noreasonexception.loadable.childs;
+
+import com.noreasonexception.datanuke.app.ValueFilter.AbstractValueFilter;
+import com.noreasonexception.datanuke.app.threadRunner.ThreadRunnerTaskEventsDispacher;
+import com.noreasonexception.loadable.base.HtmlParser;
+
+import java.util.regex.Pattern;
+
+public class A32_EcEuropa_GDP_EUR extends HtmlParser {
+    public A32_EcEuropa_GDP_EUR(ThreadRunnerTaskEventsDispacher disp, AbstractValueFilter<Double> valueFilter) {
+        super(disp, valueFilter);
+    }
+
+    @Override
+    protected Pattern onPatternLoad() {
+        return null;
+    }
+
+    @Override
+    protected String onUrlLoad() {
+        return null;
+    }
+
+    @Override
+    protected Double onValueExtract(String tmpString) {
+        return null;
+    }
+}
