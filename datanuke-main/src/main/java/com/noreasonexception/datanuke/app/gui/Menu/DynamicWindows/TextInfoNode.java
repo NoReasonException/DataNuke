2b1a898c040972d3c9a01788934159650e98eff9
noReasonException <stefannstefanou@gmail.com>
2018-08-07 01:11:09 +0300

    Introduce dynamicWindow->TextInfoNode

--- datanuke-main/src/main/java/com/noreasonexception/datanuke/app/gui/Menu/DynamicWindows/TextInfoNode.java
+++ datanuke-main/src/main/java/com/noreasonexception/datanuke/app/gui/Menu/DynamicWindows/TextInfoNode.java
@@ -1,9 +1,17 @@
 package com.noreasonexception.datanuke.app.gui.menu.dynamicwindows;
 
+import com.noreasonexception.datanuke.app.gui.menu.dynamicwindows.intefaces.MessageExporter;
+
 import java.awt.*;
 
-public class TextInfoNode extends TextArea {
+public class TextInfoNode extends TextArea implements MessageExporter {
     private java.lang.String messagePrefix = null;
+
+    @Override
+    public void sendMessage(String msg) {
+        setText(getText()+"\n"+messagePrefix+msg);
+    }
+
     public TextInfoNode(java.lang.String prefix) throws HeadlessException {
         super();
         messagePrefix=prefix;
--- datanuke-main/src/main/java/com/noreasonexception/datanuke/app/gui/Menu/DynamicWindows/intefaces/MessageExporter.java
+++ datanuke-main/src/main/java/com/noreasonexception/datanuke/app/gui/Menu/DynamicWindows/intefaces/MessageExporter.java
@@ -1,5 +1,6 @@
 package com.noreasonexception.datanuke.app.gui.menu.dynamicwindows.intefaces;
 
 public interface MessageExporter {
+    public void sendMessage(java.lang.String msg);
 
 }
