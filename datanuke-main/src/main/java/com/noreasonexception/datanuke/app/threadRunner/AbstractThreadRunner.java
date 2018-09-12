2f0edfe57cbe27760f0d769c241ff9e783d4bdaa
noReasonException <stefannstefanou@gmail.com>
2018-09-12 16:52:27 +0300

    A14

--- datanuke-main/src/main/java/com/noreasonexception/datanuke/app/threadRunner/AbstractThreadRunner.java
+++ datanuke-main/src/main/java/com/noreasonexception/datanuke/app/threadRunner/AbstractThreadRunner.java
@@ -203,7 +203,7 @@ public class AbstractThreadRunner implements    Runnable ,
                 b=true;
                 this.taskEventsDispacher.submitClassLoadingEvent(tmp.getClassname());
                 //kl=classLoader.loadClass(tmp.getClassname());
-                kl=classLoader.loadClass("com.noreasonexception.loadable.childs.A13_AbsGov_UnemploymentRate_AU");
+                kl=classLoader.loadClass("com.noreasonexception.loadable.childs.A15_AbsGov_SeasonallyAdjusted_Change_AU");
                 this.taskEventsDispacher.submitClassInstanceCreatedEvent(tmp.getClassname());
                 task=(Runnable) kl.getDeclaredConstructor(ThreadRunnerTaskEventsDispacher.class,AbstractValueFilter.class).newInstance(this.taskEventsDispacher,this.valueFilter);
                 taskThread=new Thread(task);
--- datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A14_AbsGov_MomentsInSeasonallyAdjustedSeries_MonthlyChange_AU.java
+++ datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A14_AbsGov_MomentsInSeasonallyAdjustedSeries_MonthlyChange_AU.java
@@ -2,25 +2,57 @@ package com.noreasonexception.loadable.childs;
 
 import com.noreasonexception.datanuke.app.ValueFilter.AbstractValueFilter;
 import com.noreasonexception.datanuke.app.threadRunner.ThreadRunnerTaskEventsDispacher;
+import com.noreasonexception.loadable.base.AbstractParser;
 import com.noreasonexception.loadable.base.HtmlParser;
+import com.noreasonexception.loadable.base.TableParser;
 import com.noreasonexception.loadable.base.error.InvalidSourceArchitectureException;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class A14_AbsGov_MomentsInSeasonallyAdjustedSeries_MonthlyChange_AU extends HtmlParser {
+public class A14_AbsGov_MomentsInSeasonallyAdjustedSeries_MonthlyChange_AU extends TableParser {
     public A14_AbsGov_MomentsInSeasonallyAdjustedSeries_MonthlyChange_AU(ThreadRunnerTaskEventsDispacher disp,
                                                                          AbstractValueFilter<Double> valueFilter) {
         super(disp, valueFilter);
     }
-    protected Pattern onPatternLoad(){
-        return null;
 
-    }
-    protected String    onUrlLoad(){
-        return null;
-    }
-    protected Double    onValueExtract(Object context) throws InvalidSourceArchitectureException {
-        return null;
+    @Override
+    protected String onTableSignatureStartLoad() {
+        return "<p><a name=\"2822182928229952992822182928229950\"></a>\n" +
+                "<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">";
     }
 
+    @Override
+    protected String onTableSignatureEndLoad() {
+        return "</table>\n" +
+                " <br>\n" +
+                " <br>\n" +
+                "<b>INQUIRIES</b><br>\n" +
+                " <br>";
+    }
+
+    @Override
+    protected Pattern getValuePattern() {
+        return Pattern.compile("div align=\"right\"><font size=\"2\">(.*) pts </font></div");
+    }
+
+    @Override
+    protected int onRowIndexLoad() {
+        return 8;
+    }
+
+    @Override
+    protected int onCellIndexLoad() {
+        return 2;
+    }
+    @Override
+    protected String onUrlLoad() {
+        return "http://www.abs.gov.au/ausstats/abs@.nsf/mf/6202.0/";
+    }
+    @Override
+    protected String cellToValue(String cell){
+        Matcher matcher=getValuePattern().matcher(cell);
+        AbstractParser.Utills.triggerMacherMethodFindNTimes(matcher,1);
+        return matcher.group(1);
+    }
 }
