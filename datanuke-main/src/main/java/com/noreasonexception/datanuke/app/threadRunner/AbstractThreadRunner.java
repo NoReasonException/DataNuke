8e13c3c687049e6703d0491b9595292b40024223
noReasonException <stefannstefanou@gmail.com>
2018-09-17 01:48:11 +0300

    fixes

--- datanuke-main/src/main/java/com/noreasonexception/datanuke/app/threadRunner/AbstractThreadRunner.java
+++ datanuke-main/src/main/java/com/noreasonexception/datanuke/app/threadRunner/AbstractThreadRunner.java
@@ -196,14 +196,10 @@ public class AbstractThreadRunner implements    Runnable ,
                             tmp.getInterval()));
             try{
                 logMessageExporter.sendMessage("will wait "+getWaitTime(tmp)/1000/60+" min(s) "+tmp.getClassname()+" )");
-
-                if(b){
-                    wait(getWaitTime(tmp));
-                }
-                b=true;
+                wait(getWaitTime(tmp));
                 this.taskEventsDispacher.submitClassLoadingEvent(tmp.getClassname());
-                //kl=classLoader.loadClass(tmp.getClassname());
-                kl=classLoader.loadClass("com.noreasonexception.loadable.childs.A22_OnsGov_RetailSalesRevisionsTriangles_UK");
+                kl=classLoader.loadClass(tmp.getClassname());
+                //kl=classLoader.loadClass("com.noreasonexception.loadable.childs.A22_OnsGov_RetailSalesRevisionsTriangles_UK");
                 this.taskEventsDispacher.submitClassInstanceCreatedEvent(tmp.getClassname());
                 task=(Runnable) kl.getDeclaredConstructor(ThreadRunnerTaskEventsDispacher.class,AbstractValueFilter.class).newInstance(this.taskEventsDispacher,this.valueFilter);
                 taskThread=new Thread(task);
