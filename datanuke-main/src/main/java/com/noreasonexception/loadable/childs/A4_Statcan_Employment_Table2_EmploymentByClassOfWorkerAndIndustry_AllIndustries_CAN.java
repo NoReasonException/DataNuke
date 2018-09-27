e8756fc7f7b82137c3c849d407509dc6e0646f3a
noReasonException <stefannstefanou@gmail.com>
2018-09-27 20:09:38 +0100

    A1-A8 Default implementations (UC)

--- datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A4_Statcan_Employment_Table2_EmploymentByClassOfWorkerAndIndustry_AllIndustries_CAN.java
+++ datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A4_Statcan_Employment_Table2_EmploymentByClassOfWorkerAndIndustry_AllIndustries_CAN.java
@@ -2,25 +2,25 @@ package com.noreasonexception.loadable.childs;
 
 import com.noreasonexception.datanuke.app.ValueFilter.AbstractValueFilter;
 import com.noreasonexception.datanuke.app.threadRunner.ThreadRunnerTaskEventsDispacher;
+import com.noreasonexception.loadable.base.CanStatParser;
 import com.noreasonexception.loadable.base.HtmlParser;
 import com.noreasonexception.loadable.base.error.InvalidSourceArchitectureException;
 
 import java.util.regex.Pattern;
 
-public class A4_Statcan_Employment_Table2_EmploymentByClassOfWorkerAndIndustry_AllIndustries_CAN extends HtmlParser {
+public class A4_Statcan_Employment_Table2_EmploymentByClassOfWorkerAndIndustry_AllIndustries_CAN extends CanStatParser {
     public A4_Statcan_Employment_Table2_EmploymentByClassOfWorkerAndIndustry_AllIndustries_CAN
             (ThreadRunnerTaskEventsDispacher disp, AbstractValueFilter<Double> valueFilter) {
         super(disp, valueFilter);
     }
-
-    protected Pattern onPatternLoad(){
-        return null;
-
+    @Override
+    protected String onEventNameLoad() {
+        return "Building permits";
     }
-    protected String         onUrlLoad(){
-        return null;
-    }
-    protected Double         onValueExtract(Object context) throws InvalidSourceArchitectureException {
-        return null;
+
+    @Override
+    protected Double onValueExtract(Object context) throws InvalidSourceArchitectureException {
+        System.out.println(".onValueExtract(Object context) called");
+        return 0d;
     }
 }
