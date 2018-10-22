5d9d7071dabcba3741cb2edfccb4ecf266a3c763
noReasonException <stefannstefanou@gmail.com>
2018-10-22 18:55:47 +0100

    small fix on A20 -> 403 error due to user-agent non-set field

--- datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A20_OnsGov_CpiAnnualRate_ChangeOver12Months_UK.java
+++ datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A20_OnsGov_CpiAnnualRate_ChangeOver12Months_UK.java
@@ -4,6 +4,8 @@ import com.noreasonexception.datanuke.app.ValueFilter.AbstractValueFilter;
 import com.noreasonexception.datanuke.app.threadRunner.ThreadRunnerTaskEventsDispacher;
 import com.noreasonexception.loadable.base.CsvParser;
 import com.noreasonexception.loadable.base.error.InvalidSourceArchitectureException;
+import com.noreasonexception.loadable.base.error.NoUserAgentRequired;
+import com.noreasonexception.loadable.base.requestParserEtc.UserAgent;
 
 import java.util.regex.Pattern;
 
@@ -16,6 +18,12 @@ public class A20_OnsGov_CpiAnnualRate_ChangeOver12Months_UK extends CsvParser {
     protected String    onUrlLoad(){
         return "https://www.ons.gov.uk/generator?format=csv&uri=/economy/inflationandpriceindices/timeseries/l55o/mm23";
     }
+
+    @Override
+    protected UserAgent onUserAgentFieldLoad() throws NoUserAgentRequired {
+        return UserAgent.DATANUKE;
+    }
+
     protected int onCsvValueIndexLoad(int numberOfValues){
         return numberOfValues-1;
     }
