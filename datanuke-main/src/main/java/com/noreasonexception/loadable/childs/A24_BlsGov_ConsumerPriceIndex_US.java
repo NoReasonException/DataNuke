12756649592623475b65526145a7e6a7a9903eca
noReasonException <stefannstefanou@gmail.com>
2018-12-20 11:10:56 +0000

    A24 Added

--- datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A24_BlsGov_ConsumerPriceIndex_US.java
+++ datanuke-main/src/main/java/com/noreasonexception/loadable/childs/A24_BlsGov_ConsumerPriceIndex_US.java
@@ -3,28 +3,45 @@ package com.noreasonexception.loadable.childs;
 import com.noreasonexception.datanuke.app.saverequestfilterhandler.SaveRequestFilterHandler;
 import com.noreasonexception.datanuke.app.threadRunner.ThreadRunnerTaskEventsDispacher;
 import com.noreasonexception.loadable.base.HtmlParser;
+import com.noreasonexception.loadable.base.TableParser;
 import com.noreasonexception.loadable.base.error.InvalidSourceArchitectureException;
+import javafx.scene.control.Tab;
 
 import java.util.regex.Pattern;
 
-public class A24_BlsGov_ConsumerPriceIndex_US extends HtmlParser {
-    public A24_BlsGov_ConsumerPriceIndex_US(ThreadRunnerTaskEventsDispacher disp,
-                                            SaveRequestFilterHandler<Double> valueFilter) {
+public class A24_BlsGov_ConsumerPriceIndex_US extends TableParser {
+    public A24_BlsGov_ConsumerPriceIndex_US(ThreadRunnerTaskEventsDispacher disp, SaveRequestFilterHandler<Double> valueFilter) {
         super(disp, valueFilter);
     }
 
     @Override
-    protected Pattern onPatternLoad() {
-        return null;
+    protected String onTableSignatureStartLoad() {
+        return "<table class=\"regular\" id=\"cpipress1\">";
+    }
+
+    @Override
+    protected String onTableSignatureEndLoad() {
+        return "</table>";
+    }
+
+    @Override
+    protected Pattern getValuePattern() {
+        return Pattern.compile("\"datavalue\">(.*?)</sp");
+    }
+
+
+    @Override
+    protected int onRowIndexLoad() {
+        return 3;
+    }
+
+    @Override
+    protected int onCellIndexLoad() {
+        return 9;
     }
 
     @Override
     protected String onUrlLoad() {
-        return null;
-    }
-
-    @Override
-    protected Double onValueExtract(Object context) throws InvalidSourceArchitectureException {
-        return null;
+        return "https://www.bls.gov/news.release/cpi.t01.htm";
     }
 }
