c2c21acffcdf7ed4d681ca91bfd9e12fc78664f5
noReasonException <stefannstefanou@gmail.com>
2018-12-29 12:03:54 +0000

    release version

--- datanuke-main/src/main/java/com/noreasonexception/datanuke/app/fileprotocol/FuseFileProtocol.java
+++ datanuke-main/src/main/java/com/noreasonexception/datanuke/app/fileprotocol/FuseFileProtocol.java
@@ -15,9 +15,9 @@ import java.util.Objects;
 
 public class FuseFileProtocol extends FuseStubFS implements ListToFileProtocol<Double>{
 
-    public static  String HELLO_PATH = "/hello";
-    public static  ArrayList<Double> HELLO_STR ;
-    private static final String DRIVE_NAME = "J:/";
+    public static  String MASTER_PATH = "/hello";
+    public static  ArrayList<Double> MASTER_LIST;
+    private static final String DRIVE_NAME = "J:\\";
 
     @Override
     public int getattr(String path, FileStat stat) {
@@ -25,10 +25,10 @@ public class FuseFileProtocol extends FuseStubFS implements ListToFileProtocol<D
         if (Objects.equals(path, "/")) {
             stat.st_mode.set(FileStat.S_IFDIR | 0755);
             stat.st_nlink.set(2);
-        } else if (HELLO_PATH.equals(path)) {
+        } else if (MASTER_PATH.equals(path)) {
             stat.st_mode.set(FileStat.S_IFREG | 0444);
             stat.st_nlink.set(1);
-            stat.st_size.set(Arrays.toString(HELLO_STR.toArray()).getBytes().length);
+            stat.st_size.set(Arrays.toString(MASTER_LIST.toArray()).getBytes().length);
         } else {
             res = -ErrorCodes.ENOENT();
         }
@@ -43,13 +43,13 @@ public class FuseFileProtocol extends FuseStubFS implements ListToFileProtocol<D
 
         filter.apply(buf, ".", null, 0);
         filter.apply(buf, "..", null, 0);
-        filter.apply(buf, HELLO_PATH.substring(1), null, 0);
+        filter.apply(buf, MASTER_PATH.substring(1), null, 0);
         return 0;
     }
 
     @Override
     public int open(String path, FuseFileInfo fi) {
-        if (!HELLO_PATH.equals(path)) {
+        if (!MASTER_PATH.equals(path)) {
             return -ErrorCodes.ENOENT();
         }
         return 0;
@@ -57,11 +57,11 @@ public class FuseFileProtocol extends FuseStubFS implements ListToFileProtocol<D
 
     @Override
     public int read(String path, Pointer buf, @size_t long size, @off_t long offset, FuseFileInfo fi) {
-        if (!HELLO_PATH.equals(path)) {
+        if (!MASTER_PATH.equals(path)) {
             return -ErrorCodes.ENOENT();
         }
 
-        byte[] bytes = Arrays.toString(HELLO_STR.toArray())
+        byte[] bytes = Arrays.toString(MASTER_LIST.toArray())
                 .replace("[","")
                 .replace("]","")
                 .getBytes();
@@ -79,20 +79,20 @@ public class FuseFileProtocol extends FuseStubFS implements ListToFileProtocol<D
 
     public FuseFileProtocol() {
         //initial status until first .saveList() call
-        HELLO_STR=new ArrayList<Double>();
-        HELLO_STR.add(0d);
-        this.mount(Paths.get("J:\\"), false, true);
+        MASTER_LIST =new ArrayList<Double>();
+        MASTER_LIST.add(0d);
+        this.mount(Paths.get(this.DRIVE_NAME), false, true);
     }
     public boolean saveList(ArrayList<Double> elementsToSave, Object[] generic_args) {
-        this.HELLO_STR=elementsToSave;
-        this.HELLO_PATH="/news"+System.currentTimeMillis()/1000+".csv";
+        this.MASTER_LIST =elementsToSave;
+        this.MASTER_PATH ="/news"+System.currentTimeMillis()/1000+".csv";
         for (int i = 0; i < elementsToSave.size(); i++) {
             if(!generic_args[0].equals(i)){
-                this.HELLO_STR.set(i,0d);
+                this.MASTER_LIST.set(i,0d);
             }
 
         }
-        System.out.println("Operation on "+HELLO_PATH+ " completed");
+        System.out.println("Operation on "+ DRIVE_NAME+MASTER_PATH + " completed");
         return true;
 
     }
